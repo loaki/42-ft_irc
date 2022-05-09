6418a1355b6c5ba9ea8cdb3676a4a9eb3f833533
Lucien-alexandre SANCHEZ-ZUBER <lucisanc@e1r9p3.clusters.42paris.fr>
2022-05-09 17:23:46 +0200

    ecole

--- inc/user.hpp
+++ inc/user.hpp
@@ -6,24 +6,25 @@
 namespace irc {
 
 class User {
-    public:
-        User(int fd);
-        ~User();
+	public:
+		User(int fd);
+		~User();
 
-        void setNickname(std::string name);
-        void setUsername(std::string name);
-        void setUserFd(int fd);
-        
-        std::string const& getNickename()const;
-        std::string const& getUsername()const;
-        int const& getUserFd()const;
+		void setNickname(std::string name);
+		void setUsername(std::string name);
+		void setUserFd(int fd);
+		void setUserId(int Id);
+		
+		std::string const& getNickename()const;
+		std::string const& getUsername()const;
+		int const& getUserFd()const;
+		int const & getUserId() const;
 
-       
-
-    private:
-        int _fd;
-        std::string _nickname;
-        std::string _username;
+	private:
+		int _fd;
+		int _id;
+		std::string _nickname;
+		std::string _username;
 
 };
 
