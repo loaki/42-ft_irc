8d56e11104c4cc1564256d024d8b0a68f0941ea2
Lucien-alexandre SANCHEZ-ZUBER <lucisanc@e1r9p2.42.fr>
2022-05-10 14:24:39 +0200

    sh2

--- proxy-irc/ergo_launch.sh
+++ proxy-irc/ergo_launch.sh
@@ -5,3 +5,5 @@ docker pull ghcr.io/ergochat/ergo:stable
 docker volume create ergo-data
 
 docker run -d --name ergo -v ergo-data:/ircd -p 6667:6667 -p 6697:6697 ghcr.io/ergochat/ergo:stable
+
+python proxy-irc/proxy.py testnet.ergo.chat 6667 8888
